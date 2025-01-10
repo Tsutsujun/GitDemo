b489a44be9bb712dd33c54138e6b2830d9665599
Tsutsujun <kattou.surumono@gmail.com>
2025-01-11 08:21:52 +0900

    Architect Asian development

--- test.java
+++ test.java
@@ -5,5 +5,15 @@ public class test {
     msg += "GitHub's ";
     msg += "World!";
     System.out.println(msg);
+
+    int odd_sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      if (i % 2 != 0) {
+        odd_sum += i;
+      } else {
+        continue;
+      }
+    }
+    System.out.println(odd_sum);
   }
 }
--- test2.java
+++ test2.java
@@ -4,5 +4,15 @@ public class test2 {
     msg += "Hello ";
     msg += "World!";
     System.out.println(msg);
+
+    int even_sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      if (i % 2 == 0) {
+        even_sum += i;
+      } else {
+        continue;
+      }
+    }
+    System.out.println(even_sum);
   }
 }
